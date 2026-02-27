54be501afc0d8d379d2608df259271665b7bc3f2
unknown <949leoni@gmail.com>
2026-02-27 15:24:33 -0300

    create a content of search

--- app/src/components/planning/Planning.jsx
+++ app/src/components/planning/Planning.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 
 function Planning() {
   return (
-    <div className='bg-gray-800 h-svh w-svw p-1 grid md:grid-cols-[1fr_500px] grid-cols-[1fr_300px] gap-4'>
-        <div className="left bg-gray-100">
-               
+    <div className='bg-gray-800 h-svh w-svw p-1 grid md:grid-cols-[1fr_500px] grid-cols-[min(400px)_300px] gap-4'>
+        <div className="left bg-gray-100 grid grid-rows-[50px_120px_1fr]">
+               <div className="bg-white ">nav</div>
+               <div className="bg-green-500">search</div>
+               <div className="bg-gray-900 flex flex-row  flex-wrap content-around justify-around items-center ">
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+                    <div className="bg-white rounded-lg h-[200px] w-[200px] m-1"></div>
+               </div>
+
         </div>
         <div className="rigth bg-blue-900">2</div>
     </div>
