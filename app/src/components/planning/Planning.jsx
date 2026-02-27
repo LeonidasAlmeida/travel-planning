ed07528eba06704a47259fb978e4967ecd4d35ca
unknown <949leoni@gmail.com>
2026-02-27 14:47:31 -0300

    create a division of planning

--- app/src/components/planning/Planning.jsx
+++ app/src/components/planning/Planning.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Planning() {
   return (
-    <div className='bg-gray-800 h-svh w-svw p-1 flex flex-col '>
-        <div className="left">1</div>
-        <div className="rigth">2</div>
+    <div className='bg-gray-800 h-svh w-svw p-1 grid md:grid-cols-[1fr_500px] grid-cols-[1fr_300px] gap-4'>
+        <div className="left bg-gray-100">
+               
+        </div>
+        <div className="rigth bg-blue-900">2</div>
     </div>
   )
 }
