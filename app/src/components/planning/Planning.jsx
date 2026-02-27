1c1ea091e9accc5045994c7089e0d52d3a5a12d7
unknown <949leoni@gmail.com>
2026-02-27 14:12:07 -0300

    create division of panning component

---
+++ app/src/components/planning/Planning.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Planning() {
+  return (
+    <div className='bg-gray-800 h-svh w-svw p-1 flex flex-col '>
+        <div className="left">1</div>
+        <div className="rigth">2</div>
+    </div>
+  )
+}
+
+export default Planning
